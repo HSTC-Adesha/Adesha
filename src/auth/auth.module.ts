b657370902c888b6fd66b4a0323f8100caaa4b30
houcemhamed <housemhamed@gmail.com>
2020-06-14 14:12:35 +0100

    commit

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,27 +1,7 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { JwtStrategy } from './strategies/jwt.strategy';
-import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
-import { MongooseModule } from '@nestjs/mongoose';
-import { UserSchema } from '../user/schemas/user.schema';
-import { RefreshTokenSchema } from './schemas/refresh-token.schema';
-import { AuthController } from './auth.controller';
-import { ForgotPasswordSchema } from '../user/schemas/forgot-password.schema';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: 'ForgotPassword', schema: ForgotPasswordSchema }]),
-    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
-    MongooseModule.forFeature([{ name: 'RefreshToken', schema: RefreshTokenSchema }]),
-    PassportModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: process.env.JWT_EXPIRATION },
-    }),
-  ],
-  controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
-  exports: [AuthService],
+  providers: [AuthService]
 })
 export class AuthModule {}
