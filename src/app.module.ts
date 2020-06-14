b3b894ede6f4da4226285408eee23082906024de
houcemhamed <housemhamed@gmail.com>
2020-06-14 14:16:21 +0100

    com

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import{MongooseModule} from '@nestjs/mongoose';
-import { ChequeModule } from "./cheque/cheque.module";
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [
-    AuthModule, 
-    UserModule, 
-    ChequeModule,
-    MongooseModule.forRootAsync({
-    useFactory: () => ({  
-      uri: 'mongodb://localhost/adesha',
-    }),
-  }),
- 
-],
-controllers: [AppController],
-providers: [AppService]
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
-
-
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { ChequeModule } from './cheque/cheque.module';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-console.log('app started')
-  const options = new DocumentBuilder()
-    .setTitle('Adesha API')
-    .setDescription('The ADESHA API description')
-    .setVersion('1.0.0')
-    .build();
-    const document = SwaggerModule.createDocument(app, options, {
-      include: [
-        AuthModule,
-        ChequeModule,
-        UserModule,
-    ],
-    });
-  SwaggerModule.setup('api', app, document);
-
   await app.listen(3000);
-  console.log('app.listen(3000)')
-
 }
 bootstrap();
