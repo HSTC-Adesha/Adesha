77c5048e38341e012cfc36d8bb75df561c6a6df5
houcemhamed <housemhamed@gmail.com>
2020-06-19 19:21:18 +0100

    app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ChequeModule } from "./cheque/cheque.module";
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, ChequeModule ],
   controllers: [AppController],
   providers: [AppService],
 })
